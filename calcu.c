c49451edca3080d981dced5e699a08f5f38d1933
jhonfredy149514 <jhonfredy149514@hotmail.com>
2015-08-17 23:26:54 -0500

    se modifico calcu.c

---
+++ calcu.c
@@ -0,0 +1,17 @@
+#include"calcu.h"
+int suma(int parametro1, int parametro2)
+{
+return parametro1+parametro2;
+}
+int resta(int parametro1, int parametro2)
+{
+return parametro1-parametro2;
+}
+int multiplicacion(int parametro1, int parametro2)
+{
+return parametro1*parametro2;
+}
+int division(int parametro1, int parametro2)
+{
+return parametro1/parametro2;
+}
