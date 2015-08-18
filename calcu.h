2278dd129b71582b8867d3854bf8886f30f3d554
jhonfredy149514 <jhonfredy149514@hotmail.com>
2015-08-17 23:16:12 -0500

    nuevo archivo calcu.h}

---
+++ calcu.h
@@ -0,0 +1,27 @@
+/**
+*\ funcion suma
+*\ingresar paratro1
+*\ingresar paratro2
+*\return retorna el resultado de la suma
+int suma(int parametro1, int parametro2);
+
+/**
+*\ funcion resta
+*\ingresar paratro1
+*\ingresar paratro2
+*\return retorna el resultado de la resta
+int resta(int parametro1, int parametro2);
+
+/**
+*\ funcion multiplicacion
+*\ingresar paratro1
+*\ingresar paratro2
+*\return retorna el resultado de la multiplicacion
+int multiplicacion(int parametro1, int parametro2);
+
+/**
+*\ funcion division
+*\ingresar paratro1
+*\ingresar paratro2
+*\return retorna el resultado de la division
+int division(int parametro1, int parametro2);
